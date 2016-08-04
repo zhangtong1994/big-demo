b8bb35c6e5773de397e96d9a3882e4d8a394f424
zhangtong1994 <602967699@qq.com>
2016-08-04 16:26:27 +0800

    update

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {Card} from 'material-ui/Card';
+import CircularProgress from 'material-ui/CircularProgress';
 class Home extends Component {
   constructor(props){
      super(props);
      this.state = {
-       resString: {}
+       resString: {},
+       wait:true
      }
   }
   componentDidMount(){
     axios.get('https://api.github.com/users/zhangtong1994')
          .then((res) => {
            this.setState({
-              resString:res.data
+              resString:res.data,
+              wait:false
            })
          })
   }
   render(){
 
+
     return(
       <div className='account'>
-        <Card className="content">
+        <Card className="content">{
+           this.state.wait ? <div>Reading.....<CircularProgress /></div> :
 
-         <img className='pic' src = {this.state.resString.avatar_url} />
-        <ul className='list'>
-         <li>
-           <p>Follows</p>
-           <p>{this.state.resString.follows}</p>
-         </li>
-         <li>
-           <p>Repos</p>
-           <p>{this.state.resString.repos}</p>
-         </li>
-         <li>
-           <p>Following</p>
-           <p>{this.state.resString.following}</p>
-         </li>
-        </ul>
+            <div>
+              <img className='pic' src = {this.state.resString.avatar_url} />
+              <ul className='list'>
+                 <li>
+                    <p>Follows</p>
+                    <p>{this.state.resString.followers}</p>
+                </li>
+                <li>
+                    <p>Repos</p>
+                    <p>{this.state.resString.public_repos}</p>
+                </li>
+                <li>
+                    <p>Following</p>
+                    <p>{this.state.resString.following}</p>
+                </li>
+              </ul>
+            </div>
+          }
        </Card>
       </div>
     );
