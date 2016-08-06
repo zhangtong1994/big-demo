8f73f2a72840627a44ec2a176ce00c68f4b36f6d
zhangtong1994 <602967699@qq.com>
2016-08-06 15:49:20 +0800

    i

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,29 +2,39 @@ import React, { Component } from 'react';
 import BlogCard from './BlogCard.js';
 import map from 'lodash/fp/map';
 import Search from './Search'
-
-let blogs=[
-  {index:'1',title:'haha',date:'2014.2.26'},
-  {index:'2',title:'first',date:'2014.2.29'},
-  {index:'3',title:'second',date:'2014.7.26'},
-  {index:'4',title:'third',date:'2014.5.26'}
-]
+import axios from 'axios';
+// let blogs=[
+//   {index:'1',title:'haha',date:'2014.2.26'},
+//   {index:'2',title:'first',date:'2014.2.29'},
+//   {index:'3',title:'second',date:'2014.7.26'},
+//   {index:'4',title:'third',date:'2014.5.26'}
+// ]
+//https://raw.githubusercontent.com/zhangtong1994/big-demo/master/posts/index.json
 class Blog extends Component {
+  constructor(){
+     super();
+     this.state = {
+       posts: ''
+
+     }
+  }
+  componentDidMount(){
+    axios.get(`https://raw.githubusercontent.com/zhangtong1994/big-demo/master/posts/index.json?v=${Math.random()}`)
+         .then((res) => {
+           this.setState({
+              posts:res.data
+
+           })
+         })
+  }
   render(){
     var blogCards = [];
-    if (this.props.search=='') {
+
     map((b) =>  {
               blogCards.push(
-                <BlogCard title={b.title} date={b.date} index={b.index} key={Math.random()}/>);},blogs)
-  }else{
-      let query=new RegExp(this.props.search,"i");
-      for (var i = 0; i < blogs.length; i++) {
-        if (query.test(blogs[i].title)) {
-          blogCards.push(<BlogCard title={blogs[i].title} date={blogs[i].date} index={blogs[i].index} key={Math.random()}/>)
-        }
-      }
+                <BlogCard title={b.title} date={b.date} index={b.index} key={Math.random()}/>);},this.state.posts)
 
-  }
+
     return(
       <div>
         <Search />
