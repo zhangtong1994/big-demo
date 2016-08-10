4cf70d7f9e89ee15a7198d71da3f836ed0ee2d28
zhangtong1994 <602967699@qq.com>
2016-08-10 21:36:39 +0800

    add works.js footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,27 @@ import React,{Component} from 'react';
 class Footer extends Component{
   render(){
     let styles = {
+      root:{
       backgroundColor:'#212121',
-
+      height:'300px',
+      textAlign:'center'
+    },
+    img:{
+      maxWidth:'200px',
       height:'200px'
+    },
+    p:{
+      color:'#ccc',
+      lineHeight:'80px'
     }
+  }
     return(
-     <div style={styles}>
-       Footer
-      </div>
+     <div style={styles.root}>
+
+             <p style={styles.p}>欢迎添加我的微信:zhangairen23</p>
+             <img style={styles.img} src="http://obo85nywl.bkt.clouddn.com/images/wechat.jpg" />
+
+    </div>
     )
   }
 }
--- src/components/Home/works.js
+++ src/components/Home/works.js
@@ -3,40 +3,40 @@ import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'mat
 class Works extends React.Component{
   render(){
     return(
-      <div>
-       <Card>
-           <CardMedia
-             overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-           >
-             <img src="../../images/Home.jpg" />
-          </CardMedia>
-       </Card>
-       <Card>
-           <CardMedia
-             overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-           >
-             <img src="../../images/Home.jpg" />
-          </CardMedia>
-       </Card>
-       <Card>
+      <div className='work-container'>
+       <Card className="work-card">
            <CardMedia
              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
            >
              <img src="https://raw.githubusercontent.com/zhangtong1994/big-demo/e1cac8ff7a97351f08c72f8734be737e9cad493a/src/images/Home.jpg" />
           </CardMedia>
        </Card>
-       <Card>
+       <Card className="work-card">
            <CardMedia
              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
            >
-             <img src="../../images/Home.jpg" />
+             <img src="https://raw.githubusercontent.com/zhangtong1994/big-demo/e1cac8ff7a97351f08c72f8734be737e9cad493a/src/images/Home.jpg" />
           </CardMedia>
        </Card>
-       <Card>
+       <Card className="work-card">
            <CardMedia
              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
            >
-             <img src="../../images/Home.jpg" />
+             <img src="https://raw.githubusercontent.com/zhangtong1994/big-demo/e1cac8ff7a97351f08c72f8734be737e9cad493a/src/images/Home.jpg" />
+          </CardMedia>
+       </Card>
+       <Card className="work-card">
+           <CardMedia
+             overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+           >
+             <img src="https://raw.githubusercontent.com/zhangtong1994/big-demo/e1cac8ff7a97351f08c72f8734be737e9cad493a/src/images/Home.jpg" />
+          </CardMedia>
+       </Card>
+       <Card className="work-card">
+           <CardMedia
+             overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+           >
+             <img src="https://raw.githubusercontent.com/zhangtong1994/big-demo/e1cac8ff7a97351f08c72f8734be737e9cad493a/src/images/Home.jpg" />
           </CardMedia>
        </Card>
       </div>
